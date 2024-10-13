5969c1a29eb4ffec78f65881134e159919bbb742
Rainer Koellee <rainer.koelle@eurocontrol.int>
2024-10-13 08:59:11 +0800

    next stab at Global Flight Time Variability

--- R/apdf-nice-names.R
+++ R/apdf-nice-names.R
@@ -6,7 +6,7 @@ make_nice_names_apdf <- function(.df){
     rn_df
 }
 
-trim_apdf_for_pbwg <- function(.df){
+trim_apdf_for_pbwg <- function(.df, .sam_id = FALSE){
    tmp <- .df |> dplyr::select(
         AP_C_FLTID, AP_C_REG, ADEP_ICAO, ADES_ICAO
         , SRC_PHASE
@@ -14,6 +14,8 @@ trim_apdf_for_pbwg <- function(.df){
         , ARCTYP, AC_CLASS, AP_C_FLTRUL, AP_C_RWY, AP_C_STND
         , C40_CROSS_TIME, C40_CROSS_LAT, C40_CROSS_LON, C40_CROSS_FL, C40_BEARING
         , C100_CROSS_TIME, C100_CROSS_LAT, C100_CROSS_LON, C100_CROSS_FL, C100_BEARING
+        , SAM_ID = IM_SAMAD_ID
         )
+   if(! .sam_id) tmp <- tmp |> dplyr::select(-SAM_ID)
    return(tmp)
 }
